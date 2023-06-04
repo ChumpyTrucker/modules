864513902a34ceb481f1b6841f9532cee4dad5f9
ChumpyTrucker <87389714+ChumpyTrucker@users.noreply.github.com>
2023-06-04 23:45:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from utility import multiply, divide
+from shopping.more_shopping import shopping_cart
+
+print(shopping_cart.buy('apple'))
+print(divide(5, 3))
+print(multiply(5, 6))
+print(max([1, 2, 3]))
