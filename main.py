501dc7688e7d9fa1aab311d3051793e7634de298
ChumpyTrucker <87389714+ChumpyTrucker@users.noreply.github.com>
2023-06-05 23:11:35 +0100

    Adding __name__ code to the project

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from utility import multiply, divide
 from shopping.more_shopping import shopping_cart
 
-print(shopping_cart.buy('apple'))
-print(divide(5, 3))
-print(multiply(5, 6))
-print(max([1, 2, 3]))
+if __name__ == '__main__':
+    print(shopping_cart.buy('apple'))
+    print(divide(5, 3))
+    print(multiply(5, 6))
+    print(max([1, 2, 3]))
+
+
+    print('please run main file')
